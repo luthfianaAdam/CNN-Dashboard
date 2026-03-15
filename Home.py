4e0a67a4c1a7e1439b84185c73684bfc32501bdb
luthfianaAdam <luthfianaadam.m@gmail.com>
2026-03-15 16:37:25 +0700

    TEST

--- Home.py
+++ Home.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("Home")
+st.title("ARTIFICIAL INTELLIGENCE")
+st.header("OUR TEAM", divider="gray")
+st.subheader("LUTHFIANA ADAM MALDINI (123456)")
+st.subheader("LUTHFIANA ADAM MALDINI ()")
+st.subheader("LUTHFIANA ADAM MALDINI ()")
+st.subheader("LUTHFIANA ADAM MALDINI ()")
+st.subheader("LUTHFIANA ADAM MALDINI ()")
